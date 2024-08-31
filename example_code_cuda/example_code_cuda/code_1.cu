74f703e62e5f5c2ff1a43ac5d3f98b839a43faba
Ameer Salam <ameer-salam@users.noreply.github.com>
2024-08-31 13:14:34 +0530

    updated program `

--- example_code_cuda/example_code_cuda/code_1.cu
+++ example_code_cuda/example_code_cuda/code_1.cu
@@ -6,13 +6,18 @@
 //declaring the kernel function
 __global__ void hello_cuda()
 {
-	printf("Hello CUDA!");
+	printf("Hello CUDA!\n");
 }
 
 int main()
 {
-	hello_cuda << <1, 1 >> > ();
+	//to print Hello CUDA! once
+	//hello_cuda << <1, 1 >> > ();s
+	//to print Hello CUDA! 30 times 
+	//we will run 30 threads in 1 block
+	hello_cuda << <1, 30 >> > ();
 	cudaDeviceSynchronize();
+
 
 	cudaDeviceReset();  //to reset the device and the memory too
 	return 0;
