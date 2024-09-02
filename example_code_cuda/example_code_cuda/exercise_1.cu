c041041f188a6aac48a47bb96d9713f42e958a7f
Ameer Salam <ameer-salam@users.noreply.github.com>
2024-09-02 06:33:07 +0530

    commented with main

--- example_code_cuda/example_code_cuda/exercise_1.cu
+++ example_code_cuda/example_code_cuda/exercise_1.cu
@@ -10,12 +10,12 @@ __global__ void exercise_program1()
 	printf("threadIdx.x : %d\tthreadIdx.y : %d\tthreadIdx.z : %d\t -- blockIdx.x : %d\tblockIdx.y : %d\tblockIdx.z : %d\t--- gridDim.x : %d\tgridDim.y : %d\tgridDim.z : %d\n", threadIdx.x, threadIdx.y, threadIdx.z, blockIdx.x, blockIdx.y, blockIdx.z, gridDim.x, gridDim.y, gridDim.z);
 }
 
-int main()
-{
-	dim3 block(2,2,2);
-	dim3 grid(2, 2, 2);
-	exercise_program1 << <grid, block>> > ();
-	cudaDeviceSynchronize();
-	cudaDeviceReset();
-	return 0;
-}
+//int main()
+//{
+//	dim3 block(2,2,2);
+//	dim3 grid(2, 2, 2);
+//	exercise_program1 << <grid, block>> > ();
+//	cudaDeviceSynchronize();
+//	cudaDeviceReset();
+//	return 0;
+//}
