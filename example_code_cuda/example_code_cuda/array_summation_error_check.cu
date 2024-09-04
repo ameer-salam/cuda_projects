c7c1576df9feb93eaf5141355b11e0a240af55a0
Ameer Salam <ameer-salam@users.noreply.github.com>
2024-09-04 19:53:22 +0530

    updates

---
+++ example_code_cuda/example_code_cuda/array_summation_error_check.cu
@@ -0,0 +1,98 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <random>
+
+__global__ void array_summation_2(int *a, int *b, int *c, int size)
+{
+	int gid = threadIdx.x + (blockDim.x * blockIdx.x);
+	if (gid < size)
+	{
+		c[gid] = a[gid] + b[gid];
+	}
+}
+
+void array_summation_2_cpu(int *a, int *b, int *c, int size)
+{
+	printf("CPU Summation function called!\n");
+	for (int i = 0; i < size; i++)
+	{
+		c[i] = a[i] + b[i];
+	}
+	//below statement is just for validation
+	//printf("CPU Summation of a1[998] : %d\t + a2[998] : %d\t is = %d\n", a[998], b[998], c[998]);
+}
+
+void array_summation_2_validation(int *a, int *b, int size)
+{
+	printf("validation function called!\n");
+	for (int i = 0; i < size; i++)
+	{
+		if (a[i] != b[i])
+		{
+			printf("They do not match!\b");
+			return;
+		}
+		else
+			printf("here is the problem\n");
+	}
+	printf("\n\nArrays match! and have been validated!\n");
+}
+
+int main()
+{
+	int array_size = 1000;
+	int array_byte_size = array_size * sizeof(int);
+	int *a1, *a2, *result, *c_results;
+	
+	//allocating memory space for the above variables a1, a2, result
+	a1 = (int*)malloc(array_byte_size);
+	a2 = (int*)malloc(array_byte_size);
+	result = (int*)malloc(array_byte_size);
+
+	//generating random numbers and storing them in the above array
+	for (int i = 0; i < array_size; i++)
+	{
+		a1[i] = rand() % 100 + 1;
+		a2[i] = rand() % 100 + 1;
+	}
+
+	//printing the randomly generated array elements
+	for (int i = 0; i < array_size; i++)
+	{
+		printf("a1[%d] : %d\ta2[%d] : %d\n", i, a1[i], i, a2[i]);
+	}
+
+	//declaring variables to store the memory addresses of the device variables
+	int *device_a1_address, *device_a2_address, *device_result_address;
+
+	//allocating the memory in the device and storing the memory in the above pointer variables
+	cudaMalloc((int**)&device_a1_address, array_byte_size);
+	cudaMalloc((int**)&device_a2_address, array_byte_size);
+	cudaMalloc((int**)&device_result_address, array_byte_size);
+
+	//after decleration of the array, copying the values from the host variable to the device variables
+	cudaMemcpy(device_a1_address, a1, array_byte_size, cudaMemcpyHostToDevice);
+	cudaMemcpy(device_a2_address, a2, array_byte_size, cudaMemcpyHostToDevice);
+
+	//declaring the grid and block variables
+	int block_size = 128;
+	dim3 block(block_size);
+	dim3 grid((array_size/block_size+1));
+
+	//launch of the kernel function
+	array_summation_2 <<< grid, block>>> (device_a1_address, device_a2_address, device_result_address, array_byte_size);
+	cudaDeviceSynchronize();
+	cudaMemcpy(result, device_result_address, array_byte_size, cudaMemcpyDeviceToHost);
+
+	//validate the cuda generated result by CPU generated result
+	c_results = (int*)malloc(array_byte_size);
+	array_summation_2_cpu(a1, a2, c_results, array_size);
+
+	//validation function
+	array_summation_2_validation(c_results, result, array_size);
+
+	return 0;
+}
--- example_code_cuda/example_code_cuda/array_summation_with_validation.cu
+++ example_code_cuda/example_code_cuda/array_summation_with_validation.cu
@@ -37,73 +37,73 @@ void validation_function(int* gpu_result, int* cpu_result, int size)
 	printf("\n\nthe arrays are same and VALIDATED!\n");
 }
 
-int main()
-{
-	int array_size = 1000;
-	int array_byte_size = array_size * sizeof(int);
-	//host pointer variables to store the values of the arrays
-	int *a1, *a2, *results;
-
-	//assigning memory space for the variables and storing the head address of the array of memory
-	a1 = (int*)malloc(array_byte_size);
-	a2 = (int*)malloc(array_byte_size);
-	results = (int*)malloc(array_byte_size);
-
-	//assigning memory space for the variables for CPU generated results
-	int* result_cpu;//, *a2_cpu, * a1_cpu;
-
-	//allocating memory location for the cpu generated arrays
-	result_cpu = (int*)malloc(array_byte_size);
-	//a2_cpu = (int*)malloc(array_byte_size);
-	//results_cpu = (int*)malloc(array_byte_size);
-
-	//rnadomly initialize the array with random function
-	for (int i = 0; i < array_size; i++)
-	{
-		a1[i] = rand() % 100+1;
-		a2[i] = rand() % 100 + 1;
-	}
-
-	//printing all the variables of a1 and a2
-	for (int i = 0; i < array_size; i++)
-	{
-		printf("a1[%d] = %d\ta2[%d] = %d\n", i, a1[i], i, a2[i]);
-	}
-	printf("\n");
-
-	//declaring pointer variables in host to store the address of the device variables
-	int *device_a1_address, *device_a2_address, *device_result_address;
-
-	//allocating memory in device for a1, a2, and results
-	cudaMalloc((int**)&device_a1_address, array_byte_size);
-	cudaMalloc((int**)&device_a2_address, array_byte_size);
-	cudaMalloc((int**)&device_result_address, array_byte_size);
-
-	//copying the  values for host variables to the device variables
-	cudaMemcpy(device_a1_address, a1, array_byte_size, cudaMemcpyHostToDevice);
-	cudaMemcpy(device_a2_address, a2, array_byte_size, cudaMemcpyHostToDevice);
-
-	//grid and block dimensions
-	int no_of_threads_per_block = 128;
-	dim3 grid((array_size/no_of_threads_per_block + 1)); //+1 is to gaurantee that we will hae more grid than needed to avoid unwanted errors
-	dim3 block(no_of_threads_per_block);
-
-	//calling the kernel function
-	array_summation_validation <<<grid, block>>> (device_a1_address, device_a2_address, device_result_address, array_size);
-	cudaDeviceSynchronize();
-
-	//performing CPU array calculation
-	array_summation_validation_cpu(a1, a2, result_cpu, array_size);
-
-	//after kernel execution, we have to transfer the results from device to the host
-	cudaMemcpy(results, device_result_address, array_byte_size, cudaMemcpyDeviceToHost);
-
-	//function to validate the CPU and GPU calculation
-	validation_function(results, result_cpu, array_size);
-
-	//freeing memory in Host and device (Resetting device)
-	cudaFree(device_a1_address); cudaFree(device_a2_address); cudaFree(device_result_address);
-	free(a1);					 free(a2);					  free(results);
-	cudaDeviceReset();
-	return 0;
-}
+//int main()
+//{
+//	int array_size = 1000;
+//	int array_byte_size = array_size * sizeof(int);
+//	//host pointer variables to store the values of the arrays
+//	int *a1, *a2, *results;
+//
+//	//assigning memory space for the variables and storing the head address of the array of memory
+//	a1 = (int*)malloc(array_byte_size);
+//	a2 = (int*)malloc(array_byte_size);
+//	results = (int*)malloc(array_byte_size);
+//
+//	//assigning memory space for the variables for CPU generated results
+//	int* result_cpu;//, *a2_cpu, * a1_cpu;
+//
+//	//allocating memory location for the cpu generated arrays
+//	result_cpu = (int*)malloc(array_byte_size);
+//	//a2_cpu = (int*)malloc(array_byte_size);
+//	//results_cpu = (int*)malloc(array_byte_size);
+//
+//	//rnadomly initialize the array with random function
+//	for (int i = 0; i < array_size; i++)
+//	{
+//		a1[i] = rand() % 100+1;
+//		a2[i] = rand() % 100 + 1;
+//	}
+//
+//	//printing all the variables of a1 and a2
+//	for (int i = 0; i < array_size; i++)
+//	{
+//		printf("a1[%d] = %d\ta2[%d] = %d\n", i, a1[i], i, a2[i]);
+//	}
+//	printf("\n");
+//
+//	//declaring pointer variables in host to store the address of the device variables
+//	int *device_a1_address, *device_a2_address, *device_result_address;
+//
+//	//allocating memory in device for a1, a2, and results
+//	cudaMalloc((int**)&device_a1_address, array_byte_size);
+//	cudaMalloc((int**)&device_a2_address, array_byte_size);
+//	cudaMalloc((int**)&device_result_address, array_byte_size);
+//
+//	//copying the  values for host variables to the device variables
+//	cudaMemcpy(device_a1_address, a1, array_byte_size, cudaMemcpyHostToDevice);
+//	cudaMemcpy(device_a2_address, a2, array_byte_size, cudaMemcpyHostToDevice);
+//
+//	//grid and block dimensions
+//	int no_of_threads_per_block = 128;
+//	dim3 grid((array_size/no_of_threads_per_block + 1)); //+1 is to gaurantee that we will hae more grid than needed to avoid unwanted errors
+//	dim3 block(no_of_threads_per_block);
+//
+//	//calling the kernel function
+//	array_summation_validation <<<grid, block>>> (device_a1_address, device_a2_address, device_result_address, array_size);
+//	cudaDeviceSynchronize();
+//
+//	//performing CPU array calculation
+//	array_summation_validation_cpu(a1, a2, result_cpu, array_size);
+//
+//	//after kernel execution, we have to transfer the results from device to the host
+//	cudaMemcpy(results, device_result_address, array_byte_size, cudaMemcpyDeviceToHost);
+//
+//	//function to validate the CPU and GPU calculation
+//	validation_function(results, result_cpu, array_size);
+//
+//	//freeing memory in Host and device (Resetting device)
+//	cudaFree(device_a1_address); cudaFree(device_a2_address); cudaFree(device_result_address);
+//	free(a1);					 free(a2);					  free(results);
+//	cudaDeviceReset();
+//	return 0;
+//}
